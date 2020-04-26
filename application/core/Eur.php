44cfcd79c2d05f1f62f1cd7ef9422a51c798179c
/usr/bin/vi <support@beget.ru>
2020-04-26 06:02:44 +0300

    upload to git

---
+++ application/core/Eur.php
@@ -0,0 +1,67 @@
+<?
+
+namespace application\core;
+
+class Eur{
+	public function __construct() {
+		//echo 'that is class Eur';
+	}
+	public function get_value($code, $date_to_query){
+		//as a default cyrrency for today
+		//$yesterday  = date("d/m/Y", mktime(0, 0, 0, date("m"), date("d")-2, date("Y")));
+		$today  = $date_to_query;
+		$yesterday = explode("/", $today);
+		if((int)$yesterday[0] <= 9 and (int)$yesterday != 1){
+			$zero = '0';
+			$day = 1;
+		}
+		// exception for the start month in date
+		else if((int)$yesterday[0] == 1){
+			$yesterday = date("t/m/Y", mktime(0, 0, 0, $yesterday[1]-1, $yesterday[0], $yesterday[2]));
+			$yesterday = explode("/", $yesterday);
+			$day = 0;
+			$zero = '';
+		}
+		else{
+			$zero = '';
+			$day = 1;
+		}
+		$yesterday[0] = (int)$yesterday[0] - $day;
+		$yesterday = $zero.$yesterday[0]."/".$yesterday[1]."/".$yesterday[2];		
+		//query for xml to api cbr.ru
+		$query_yesterday = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=$yesterday&date_req2=$yesterday&VAL_NM_RQ=$code";
+		$query_today = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=$today&date_req2=$today&VAL_NM_RQ=$code";
+		//take result for query date and one day before
+		$result_yesterday = simplexml_load_file("$query_yesterday");
+		$result_today = simplexml_load_file("$query_today");
+		//take values from result
+		$value_yesterday = $result_yesterday->Record->Value[0];
+		$value_yesterday = str_replace(',', '.', $value_yesterday);
+		$value_yesterday = (float)$value_yesterday;
+		$value_today = $result_today->Record->Value[0];
+		$value_today = str_replace(',', '.', $value_today);
+		$value_today = (float)$value_today;
+		//math a at query day value and value at one day before
+		if($value_today > $value_yesterday){
+			$ico = 'level-up'; // the icon if the currency has been increaseble
+		}
+		else if($value_today < $value_yesterday){
+			$ico = 'level-down'; // the icon if the currency has been decreaseble
+		}
+		else{
+			$ico = 'exchange'; // the icon if the currency doesn't be changed
+		}
+		$this->show($value_today, $ico);
+
+	}
+	public function show($value, $ico){
+		echo $value;
+		echo <<<HTML
+		<i class="logo fa fa-$ico">
+		</i>
+HTML;
+
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,116 @@
+<html>
+<head>
+<!-- meta -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Bootstrap -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!-- jquery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Local CSS -->
+<link rel="stylesheet" type="text/css" href="/public/styles/style.css">
+</head>
+<body>
+<?
+
+/**
+*	Require debug
+*/
+require 'application/lib/Dev.php';
+
+/**
+*	Autoloadutoload classes
+*/
+use application\core\Usd;
+use application\core\Eur;
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+	if (file_exists($path)){
+		require $path;
+	}
+});
+
+/**
+* Set default vars
+*/
+$usd_code = "R01235"; // USD code for api
+$eur_code = "R01239"; // EUR code for api
+$max_date = date("Y-m-d"); // max date in input for query, set equal to today
+
+/**
+*	Check isset date_to_query variable by user
+*/
+if($_GET['date_to_query']){
+	$date_to_query = $_GET['date_to_query']; // YES -> TAKE A GET VARIABLE
+	$date_to_query = str_replace('-', '/', $date_to_query); // replace '-' to '/'
+	$array = explode("/", $date_to_query); // make a array
+	$date_to_query = "$array[2]/$array[1]/$array[0]"; // swap the Y-m-d to d-m-Y
+}
+else{
+	$date_to_query = date("d/m/Y", mktime(0, 0, 0, date("m"), date("d")-1, date("Y"))); // NOPE -> TAKE A CURRENT DATE
+}
+
+?>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent float-right">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form class="form-inline my-2 my-lg-0" action="index.php">
+      <input type="date" name="date_to_query" class="form-control" id="date_to_query" max="<?=$max_date?>">
+      <button class="btn btn-outline-light mx-2 my-2 my-sm-0" type="submit">magic</button>
+    </form>
+  </div>
+</nav>
+<!-- container -->
+<div class="container">
+	<div class="row align-items-center" style="height: 80%; color: white;">
+		<!-- usd div -->
+		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+			
+			<div class="box-part text-center">
+				
+				<i class="fa fa-usd fa-3x" aria-hidden="true"></i>
+				
+				<div class="title mt-3">
+					<h4>USD</h4>
+				</div>
+				
+				<div class="text">
+					<span>
+					<?
+							$Usd = new Usd;
+							$Usd->get_value($usd_code, $date_to_query);
+					?>
+					</span>
+				</div>				
+			 </div>
+		</div>
+		<!-- eur div -->
+		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+	   
+			<div class="box-part text-center">
+				
+				<i class="fa fa-eur fa-3x" aria-hidden="true"></i>
+			
+				<div class="title mt-3">
+					<h4>EUR</h4>
+				</div>
+				
+				<div class="text">
+					<span>
+					<?
+							$Eur = new Eur;
+							$Eur->get_value($eur_code, $date_to_query);
+					?>
+					</span>
+				</div>
+			 </div>
+		</div>	
+	</div>
+</div>
+
+
+</body>
+</html>
